7bfd15e5fd1648947cd2994157348ce6774e4d9b
Tim <tim.r.burr@gmail.com>
2023-10-27 09:51:44 -0600

    fixed booking dropdown

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
     return (
         <div id='contact' className='max-w-[1140px] m-auto w-full px-4 py-16'>
             <h2 className='text-center text-gray-700'>Send us a message</h2>
-            <p className='text-center text-gray-700 py-2'>We$apos;re standing by!</p>
+            <p className='text-center text-gray-700 py-2'>We&apos;re standing by!</p>
             <div className='grid md:grid-cols-2 '>
                 <img className='w-full md:h-full object-cover p-2 max-h-[500px] h-[200px]' src='https://images.unsplash.com/photo-1538964173425-93884d739596?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80' alt='/'></img>
                 <form>
